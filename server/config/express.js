6c01b092cff3ed398311c97438a068bf6e34bd1a
ZhichengChen <czc555@gmail.com>
2018-01-02 15:51:00 +0800

    cancel grunt

--- server/config/express.js
+++ server/config/express.js
@@ -29,9 +29,9 @@ module.exports = function(app) {
   app.use(methodOverride());
   app.use(cookieParser());
   if ('production' === env) {
-    // app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, 'public')));
-    app.set('appPath', path.join(config.root, 'public'));
+    app.use(favicon(path.join(config.root, 'client', 'favicon.ico')));
+    app.use(express.static(path.join(config.root, 'client')));
+    app.set('appPath', path.join(config.root, 'client'));
     app.use(morgan('dev'));
   }
 
